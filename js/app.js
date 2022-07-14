19b9b41739712064a7d6cfe1ca093e1206ec9411
Aashar Mehmood <aasharmehmood3@gmail.com>
2022-07-14 18:54:28 +0500

    init

---
+++ js/app.js
@@ -0,0 +1,224 @@
+const data = `<div class="container" data-aos="fade-right">
+<img src="images/weatherApp-min.jpg" alt="Weather Checking App" />
+<div class="desc">
+  <h2>Weather App</h2>
+  <h3>Weather App using open Weather API</h3>
+  <ul>
+    <li>Responsive Design</li>
+    <li>Changes background based on temperature values</li>
+    <li>Also shows longitudes and latitudes</li>
+    <li>
+      User can enter as well as dirctly select the area from a
+      dropdown
+    </li>
+  </ul>
+  <div class="btns">
+    <a href="https://github.com/Aashar-Mehmood/JavaScript-WeatherApp"
+      >View Code</a
+    >
+    <a href="https://youtu.be/kSoPUyBrwDM">Watch Demo</a>
+  </div>
+</div>
+</div>
+<div class="container" data-aos="fade-left">
+<img src="images/calculator-min.jpg" alt="JavaScript calculator" />
+<div class="desc">
+  <h2>Calculator App</h2>
+  <h3>Simple JavaScript Calculator</h3>
+  <ul>
+    <li>Implemented using two different approaches</li>
+    <li>Responsive Design</li>
+    <li>Functionality of delete and clear</li>
+    <li>Used HTML, CSS and JavaScript</li>
+  </ul>
+  <div class="btns">
+    <a href="https://github.com/Aashar-Mehmood/JavaScript-Calculator"
+      >View Code</a
+    >
+    <a href="https://aashar-mehmood.github.io/JavaScript-Calculator"
+      >Launch</a
+    >
+  </div>
+</div>
+</div>
+
+<div class="container" data-aos="fade-right">
+<img src="images/crowdFunding-min.jpg" alt="Crowd Funding Website" />
+<div class="desc">
+  <h2>Crowd Funding Website</h2>
+  <h3>Website challenge from Frontend Mentor</h3>
+  <ul>
+    <li>Responsive design</li>
+    <li>Pledge selection view on click</li>
+    <li>Toggle whether or not the product is bookmarked</li>
+    <li>Used HTML, CSS and little bit JavaScript</li>
+  </ul>
+
+  <div class="btns">
+    <a href="https://github.com/Aashar-Mehmood/Crowd-Funding-Website"
+      >View Code</a
+    >
+    <a href="https://aashar-mehmood.github.io/Crowd-Funding-Website"
+      >Launch</a
+    >
+  </div>
+</div>
+</div>
+
+<div class="container" data-aos="fade-left">
+<img src="images/slider-min.jpg" alt="Responsive Image Slider" />
+<div class="desc">
+  <h2>Image Slider</h2>
+  <ul>
+    <li>Fully Responsive design</li>
+    <li>Enable/Disable auto scroll option</li>
+    <li>Include controls for next slides</li>
+    <li>
+      Used transform and opacity properties for transition effects
+    </li>
+    <li>Made with HML, CSS and JavaScript</li>
+  </ul>
+  <div class="btns">
+    <a href="https://github.com/Aashar-Mehmood/JavaScript-SlideShow"
+      >View Code</a
+    >
+    <a href="https://aashar-mehmood.github.io/JavaScript-SlideShow"
+      >Launch</a
+    >
+  </div>
+</div>
+</div>
+
+<div class="container" data-aos="fade-up">
+<img
+  src="images/toggleNavigation-min.jpg"
+  alt="Toggleable navigation bar"
+/>
+<div class="desc">
+  <h2>Toggleable navigation bar</h2>
+  <ul>
+    <li>A top navigation bar</li>
+    <li>Changing main area content</li>
+    <li>Changing title, description and Background images</li>
+    <li>Fully responsive and custom design</li>
+    <li>Used JavaScript objects to achive this.</li>
+  </ul>
+  <div class="btns">
+    <a
+      href="https://github.com/Aashar-Mehmood/JavaScript-ToggleNavBar"
+      >View Code</a
+    >
+    <a href="https://aashar-mehmood.github.io/JavaScript-ToggleNavBar"
+      >Launch</a
+    >
+  </div>
+</div>
+</div>`;
+
+const data1 = `<h2>Some of Project Samples</h2>
+<div class="container" data-aos="fade-right">
+  <img src="images/starshare.png" alt="Star Share" />
+  <div class="desc">
+    <h2>StarShare</h2>
+    <h3>Music Platform for stars, organizers and suppliers</h3>
+    <ul>
+      <li>Signup/Login Functionality with admin Panel</li>
+      <li>Songs buy, sell, upload, edit, unpublish features</li>
+      <li>Organize events, invite stars and suppliers</li>
+      <li>Buy/sell tickets and musical instruments</li>
+      <li>View, edit profile</li>
+      <li>Send quotes to suppliers and stars</li>
+    </ul>
+
+    <div class="btns">
+      <a href="http://starshare.infinityfreeapp.com/starshare/"
+        >Launch</a
+      >
+    </div>
+  </div>
+</div>
+<div class="container" data-aos="fade-left">
+  <img src="images/blogWebsite.png" alt="Blog Website" />
+  <div class="desc">
+    <h2>Blog Website</h2>
+    <h3>Dynamic Blog Website using PHP, MYSQL, jQuery</h3>
+    <ul>
+      <li>Signup/Login Functionality</li>
+      <li>Server Side Form Validation</li>
+      <li>Show More/Few Blogs</li>
+      <li>Create Blogs after Logging In</li>
+    </ul>
+
+    <div class="btns">
+      <a href="https://github.com/Aashar-Mehmood/Dynamic-Blog-Website"
+        >View Code</a
+      >
+      <a href="https://youtu.be/sGFQVU3JL7w">Watch Demo</a>
+    </div>
+  </div>
+</div>
+<div class="container" data-aos="fade-right">
+  <img src="images/fetchMovies-min.jpg" alt="Movies Fetching Website" />
+  <div class="desc">
+    <h2>Fetch Trending Movies</h2>
+    <h3>Movies Website using The Movie Database API</h3>
+    <ul>
+      <li>Fetches daily as well as weekly trending Movies</li>
+      <li>Iterates over result and creates DOM Elements</li>
+      <li>Also loads poster of movies using movies id</li>
+      <li>Watch trailer of particular Movie</li>
+    </ul>
+
+    <div class="btns">
+      <a
+        href="https://github.com/Aashar-Mehmood/Movie-Database-API-Project"
+        >View Code</a
+      >
+      <a href="https://youtu.be/34QKTBYloio">Watch Demo</a>
+    </div>
+  </div>
+</div>
+<div class="container" data-aos="fade-left">
+  <img src="images/weatherApp-min.jpg" alt="Weather Checking App" />
+  <div class="desc">
+    <h2>Weather App</h2>
+    <h3>Weather App using open Weather API</h3>
+    <ul>
+      <li>Responsive Design</li>
+      <li>Changes background based on temperature values</li>
+      <li>Also shows longitudes and latitudes</li>
+      <li>
+        User can enter as well as dirctly select the area from a
+        dropdown
+      </li>
+      <li style="visibility: hidden">.</li>
+    </ul>
+    <div class="btns">
+      <a href="https://github.com/Aashar-Mehmood/JavaScript-WeatherApp"
+        >View Code</a
+      >
+      <a href="https://youtu.be/kSoPUyBrwDM">Watch Demo</a>
+    </div>
+  </div>
+</div>`;
+
+const moreBtn = document.getElementById("moreProj");
+const lessBtn = document.getElementById("lessProj");
+const container = document.getElementById("projects");
+
+moreBtn.addEventListener("click", showMore);
+lessBtn.addEventListener("click", showLess);
+
+function showMore() {
+  container.innerHTML += data;
+  container.scrollBy(0, 20);
+  moreBtn.style.display = "none";
+  lessBtn.style.display = "block";
+}
+
+function showLess() {
+  container.innerHTML = data1;
+  container.scrollIntoView();
+  moreBtn.style.display = "block";
+  lessBtn.style.display = "none";
+}
